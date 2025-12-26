419d450ad82ea76f4ed7dd925cdedd851f33f170
Nidhekshaa <nknidhekshaa@gmail.com>
2025-12-26 16:02:41 +0530

    case change

--- frontend/src/HeartForm.jsx
+++ frontend/src/HeartForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./heartForm.css";
+import "./heartform.css";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
